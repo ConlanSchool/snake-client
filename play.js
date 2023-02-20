56066504036d809549d4f5c0ad8d0d3086529d6b
ConlanSchool <conlan.school@gmail.com>
2023-02-20 13:31:23 -0800

    Add a connect function

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "172.17.150.148",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server: ", data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
