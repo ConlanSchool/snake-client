484c164f8021e6a31afa592c74f15a2a3eb4eb75
ConlanSchool <conlan.school@gmail.com>
2023-02-22 15:42:09 -0800

    Add program stop with ctrl + c

--- play.js
+++ play.js
@@ -1,1 +1,19 @@
 const connect = require("./client.js");
+
+const setupInput = function () {
+  stdin.on("data", handleUserInput);
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    process.exit();
+  } else {
+    conn.write(data);
+  }
+};
