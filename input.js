003a298d75f136a354fced136dfe307596b37280
ConlanSchool <conlan.school@gmail.com>
2023-02-24 13:07:29 -0800

    Add ability to send messages

--- input.js
+++ input.js
@@ -1,20 +1,55 @@
-const connect = require("./client.js");
+const { connect } = require("./client");
+let connection;
 
-const setupInput = function () {
-  stdin.on("data", handleUserInput);
+const setupInput = function (conn) {
+  connection = conn;
 
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
+let upIntervalId;
+let leftIntervalId;
+let downIntervalId;
+let rightIntervalId;
+
 const handleUserInput = function (data) {
+  clearInterval(upIntervalId);
+  clearInterval(leftIntervalId);
+  clearInterval(downIntervalId);
+  clearInterval(rightIntervalId);
+
   if (data === "\u0003") {
     process.exit();
+  } else if (data === "w") {
+    upIntervalId = setInterval(() => {
+      connection.write("Move: up");
+    }, 100);
+  } else if (data === "a") {
+    leftIntervalId = setInterval(() => {
+      connection.write("Move: left");
+    }, 100);
+  } else if (data === "s") {
+    downIntervalId = setInterval(() => {
+      connection.write("Move: down");
+    }, 100);
+  } else if (data === "d") {
+    rightIntervalId = setInterval(() => {
+      connection.write("Move: right");
+    }, 100);
   } else {
-    conn.write(data);
+    connection.write(data);
+  }
+
+  //sssecret messages
+  if (data === "l") {
+    connection.write("LUL");
+  } else if (data === "f") {
+    connection.write("forsen");
   }
 };
 
